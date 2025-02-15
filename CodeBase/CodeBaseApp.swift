ec08d77bad9e73ffdd2581c12a1b1d039711d604
Alikhan <alikhan.mukayev@gmail.com>
2025-02-15 13:52:20 +0500

    Initial Commit

---
+++ CodeBase/CodeBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBaseApp.swift
+//  CodeBase
+//
+//  Created by Алихан  on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBaseTests/CodeBaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBaseTests.swift
+//  CodeBaseTests
+//
+//  Created by Алихан  on 15.02.2025.
+//
+
+import Testing
+@testable import CodeBase
+
+struct CodeBaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
